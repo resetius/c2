34b392fcd7bfd5ac2b2a8c24c71b47821637d1c0
manwe@R500-UCP21R <manwe@R500-UCP21R>
2008-06-02 20:46:38 +0400

    --

--- fractal/c2_lsystem.cpp
+++ fractal/c2_lsystem.cpp
@@ -1,9 +1,25 @@
+#include <stdio.h>
+extern "C" {
 #include "c2_lsystem_scanner.h"
+}
 #include "c2_lsystem_parser.hpp"
 #include "c2_lsystem.h"
 
-int main()
+int main(int argc, char * argv[])
 {
 	Parser p;
+	FILE * f = 0;
+
+	if (argc > 1) {
+		f = fopen(argv[1], "r");
+	}
+
+	if (f) yyrestart(f);
+	
+	while (yyparse(&p));
+	p.check();
+	p.print();
+
+	if (f) fclose(f);
 }
 
--- fractal/c2_lsystem.h
+++ fractal/c2_lsystem.h
@@ -2,18 +2,67 @@
 #define LSYSTEM_H
 #include <string>
 #include <map>
+#include <set>
+#include <iostream>
 
 struct Parser
 {
-  double alpha;
-  double theta;
+	bool error;
+	double alpha;
+	double theta;
     std::string axiom;
     std::map < char, std::string > r;
-  typedef std::map < char, std::string >::iterator iterator;
+	std::set < char > v;
 
-    Parser ():alpha (0.0), theta (0.0)
-  {
-  }
+	typedef std::map < char, std::string >::iterator iterator;
+
+	Parser (): error(false), alpha (0.0), theta (0.0) {
+	}
+
+	void setAxiom(const std::string & a) {
+		axiom = a;
+		for (uint i = 0; i < a.length(); ++i) {
+			if (a[i] != '[' && a[i] != ']' && a[i] != '+' && a[i] != '-') {
+				v.insert(a[i]);
+			}
+		}
+	}
+
+	bool check() {
+		if (axiom.empty()) {
+			error = true;
+			goto fail;
+		}
+
+		if (theta == 0.0) {
+			error = true;
+			goto fail;
+		}
+		
+		for (std::set < char >::iterator it = v.begin(); it != v.end(); ++it) {
+			if (r.find(*it) == r.end()) {
+				error = true;
+				goto fail;
+			}
+		}
+
+	fail:
+		return !error;
+	}
+
+	void print() {
+		std::cerr << "error : " << error << "\n";
+		std::cerr << "alpha : " << alpha << "\n";
+		std::cerr << "theta : " << theta << "\n";
+		std::cerr << "axiom : " << axiom << "\n";
+	
+		for (iterator it = r.begin(); it != r.end(); ++it) {
+			std::cerr << it->first << " : " << it->second << "\n";
+		}
+	}
 };
+
+int yyparse(Parser * );
+
 #endif //LSYSTEM_H
 
